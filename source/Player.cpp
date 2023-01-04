b44d9ef3049fbfca2055ba5faf9c267afc2bb922
Curtis <curtisdslone@gmail.com>
2023-01-04 20:32:54 +0100

    View movement good

--- source/Player.cpp
+++ source/Player.cpp
@@ -11,25 +11,37 @@ void Player::key(Event& e)
 				m_frame.y=(Direction::UP*65);
 				m_frame.x = animate.updateFrame(m_size.y,p_dt);
 				p_sprite.setTextureRect(IntRect(m_frame,m_size));
-				p_sprite.move(0,-4);
+				if (p_sprite.getPosition().y > 10)
+					p_sprite.move(0,-4);
+				if ((p_sprite.getPosition().y <= playerView.getCenter().y - containerSize.y/2) && (p_sprite.getPosition().y > 150))
+					playerView.move(0,-4);
 				break;
 			case Keyboard::Down:
 				m_frame.y=(Direction::DOWN*65);
 				m_frame.x = animate.updateFrame(m_size.y,p_dt);
 				p_sprite.setTextureRect(IntRect(m_frame,m_size));
-				p_sprite.move(0,4);
+				if (p_sprite.getPosition().y < 5920)
+					p_sprite.move(0,4);
+				if ((p_sprite.getPosition().y >= playerView.getCenter().y + containerSize.y/2) && (p_sprite.getPosition().y < 5800))
+					playerView.move(0,4);
 				break;
 			case Keyboard::Left:
 				m_frame.y=(Direction::LEFT*65);
 				m_frame.x = animate.updateFrame(m_size.y,p_dt);
 				p_sprite.setTextureRect(IntRect(m_frame,m_size));
-				p_sprite.move(-4,0);
+				if (p_sprite.getPosition().x > 10)
+					p_sprite.move(-4,0);
+				if ((p_sprite.getPosition().x <= playerView.getCenter().x - containerSize.x/2) && (p_sprite.getPosition().x > 198))
+					playerView.move(-4,0);
 				break;
 			case Keyboard::Right:
 				m_frame.y=(Direction::RIGHT*65);
 				m_frame.x = animate.updateFrame(m_size.y,p_dt);
 				p_sprite.setTextureRect(IntRect(m_frame,m_size));
-				p_sprite.move(4,0);
+				if (p_sprite.getPosition().x < 6180)
+					p_sprite.move(4,0);
+				if ((p_sprite.getPosition().x >= playerView.getCenter().x + containerSize.x/2) && (p_sprite.getPosition().x < 6050))
+					playerView.move(4,0);
 				break;
 	}
 	
@@ -37,16 +49,6 @@ void Player::key(Event& e)
 
 void Player::refresh()
 {
-	if(
-		(p_sprite.getPosition().x >= playerView.getCenter().x + containerSize.x/2) && (p_sprite.getPosition().x < 6050) ||
-		(p_sprite.getPosition().x <= playerView.getCenter().x - containerSize.x/2) && (p_sprite.getPosition().x > 198) ||
-		(p_sprite.getPosition().y >= playerView.getCenter().y + containerSize.y/2) && (p_sprite.getPosition().y < 5800)||
-		(p_sprite.getPosition().y <= playerView.getCenter().y - containerSize.y/2) && (p_sprite.getPosition().y > 150)
-	)
-	{
-		playerView.move((p_sprite.getPosition() - playerView.getCenter()) * 0.0175f);
-	}
-	
 	p_gridLoc = Vector2i((int)(playerView.getCenter().x/650),(int)(playerView.getCenter().y/650));
 	g_window.setView(playerView);
 	g_window.draw(p_sprite);
